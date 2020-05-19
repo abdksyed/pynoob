2762590aa8c40c39eeb9fe68b3853f8a030afc42
abksyed <abdksyed@gmail.com>
2020-05-20 00:08:51 +0530

    Pyn00b

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+    required = fh.read().splitlines()
+
+setuptools.setup(
+    name="pynoob", # Replace with your own username
+    version="0.0.7",
+    author="Syed Abdul K",
+    author_email="abdksyed@gmail.com",
+    description="A n00b DeepLearning Paclage",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/abksyed/pydeep",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=required,
+    include_package_data=True
+)
